dbf30c6ff5aaefbc52ab67593780bbf00ab4afeb
Tiffany Lee <tiffany@Tiffanyui-MacBook-Pro.local>
2020-09-19 01:27:54 +0900

    Tutorial 3

--- tiffany3123/mysite/polls/views.py
+++ tiffany3123/mysite/polls/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
+from .models import Question
 from django.shortcuts import render
+from django.http import Http404
 
 # Create your views here.
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5] #데이터 베이스에서 데이터를 가져옴
+    context = {'latest_question_list': latest_question_list} #html에 넘겨주기 위해 하나의 dictionary 로 만
+    return render(request, 'polls/index.html', context) #그거를 html 에 넘겨줌
